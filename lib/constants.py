5da7edff1ac6d46f825c9c0fc84eb99a01bc41c8
🎷🐛 Lee Ball <lee.cat.ball@gmail.com>
2020-07-05 13:42:15 -0700

    Actually add border

--- lib/constants.py
+++ lib/constants.py
@@ -17,4 +17,3 @@ CHARS_ONLY = re.compile("[^a-zA-Z]")
 BANNED_WORDS = ("rape", "nazi", "victim", "shootings", "bombing", "bombings")
 BANNED_PHRASES = ("shooting", "railway station", "rugby union", "historic district", "murder of", "killing of", "rugby player", ", baron ") # parens are back, baby  r"("
 PRONUNCIATION_OVERRIDES = (("HD", "10"), ("U.S.", "10"), ("Laos", "1"), ("vs.", "10"))
-
--- lib/images.py
+++ lib/images.py
@@ -14,6 +14,7 @@ def getLogo(title: str, compress: bool = True, format_img: bool = True):
 
     if format_img:
         _cropLogo(SCREENSHOT_PATH)
+        _addBorder(SCREENSHOT_PATH)
     if compress:
         _compressPng(SCREENSHOT_PATH)
 
@@ -30,7 +31,7 @@ def _cropLogo(path=SCREENSHOT_PATH):
 def _addBorder(path=SCREENSHOT_PATH):
     # http://www.imagemagick.org/Usage/crop/#border
     # Don't exit on subprocess error, image likely still in place and undamaged.
-    cmd = f"convert {SCREENSHOT_PATH} -quiet -bordercolor White -compose Over -border 20%x10% {SCREENSHOT_PATH}"
+    cmd = f"convert {SCREENSHOT_PATH} -quiet -bordercolor #000000 -border 20%x10% +repage {SCREENSHOT_PATH}"
     retcode = subprocess.run(cmd, shell=True).returncode
     if retcode != 0:
         sys.stderr.write(f"[ERROR]: imagemagick -border exit code {retcode}")    
