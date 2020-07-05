c53f7f6a439b6b2c24f676821bf44c1c54400e3d
🎷🐛 Lee Ball <lee.cat.ball@gmail.com>
2020-07-05 12:50:12 -0700

    Refactor to use WSS for screenshotting local webpage.

--- lib/constants.py
+++ lib/constants.py
@@ -7,16 +7,20 @@ MAX_ATTEMPTS = 1000
 MAX_STATUS_LEN = 280
 BACKOFF = 0.5
 TIMEOUT_BACKOFF = 240
-LOGO_PATH = r'/tmp/logo.png'
-SCREENSHOT_PATH = r'/tmp/screenshot.png'
-CHROME_PATH = r'google-chrome'
-KEY_PATH = r'/home/catherine_lee_ball/.keys'
-URL = 'file:///home/catherine_lee_ball/tmnt.html'
+
+# Compiled web screenshot binary: https://github.com/catleeball/WebScreenShot
+WSS = '/usr/local/share/tmnt/wss'
+# This is a slightly modified version of what's what http://glench.com/tmnt
+URL = 'file:///usr/local/share/tmnt/html/tmnt.html'
+SCREENSHOT_PATH = '/tmp/screenshot.png'
+KEY_PATH = '/usr/local/share/tmnt/.keys'
+TMNT_STRESSES = re.compile(r"1[02]1[02]1[02]1[02]")
+CHARS_ONLY = re.compile("[^a-zA-Z]")
+
 # Article titles the contain strings in BANNED_WORDS are skipped.
 # Banned words are things that are very inappropriate, or things
 # that are oversaturating the timeline, i.e. historic districts
-BANNED_WORDS = ("rape", "nazi", "victim", "shootings")
-BANNED_PHRASES = (r"(", "shooting", "railway station", "rugby union", "historic district", "murder of", "killing of", "rugby player", ", baron ")
-PRONUNCIATION_OVERRIDES = (("HD", "10"), ("U.S.", "10"), ("Laos", "1"))
-TMNT_STRESSES = re.compile(r"1[02]1[02]1[02]1[02]")
-CHARS_ONLY = re.compile("[^a-zA-Z]")
+BANNED_WORDS = ("rape", "nazi", "victim", "shootings", "bombing", "bombings")
+BANNED_PHRASES = ("shooting", "railway station", "rugby union", "historic district", "murder of", "killing of", "rugby player", ", baron ") # parens are back, baby  r"("
+PRONUNCIATION_OVERRIDES = (("HD", "10"), ("U.S.", "10"), ("Laos", "1"), ("vs.", "10"))
+
